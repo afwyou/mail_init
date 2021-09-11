92575706f3ab019fc864c75ab737ddcf57da5c09
Erwin <afwyousenge@gmail.com>
2021-09-11 20:25:08 +0800

    CSURF - 阻擋跨站攻擊實作-但網頁無法監聽

--- routes/contact.js
+++ routes/contact.js
@@ -1,23 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer')
-router.get('/', function (req, res) {
-    res.render('contact');
+var csrf = require('csurf')
+var csrfProtection = csrf({ cookie: true })//我要使用她的功能
+router.get('/', csrfProtection, function (req, res) {
+    //console.log(req.csrfToken())
+    res.render('contact', { csrfToken: req.csrfToken() });//把參數帶入
 });
 router.get('/review', function (req, res) {
     res.render('contactReview');
 });
-router.post('/post', function (req, res) {
+router.post('/post', csrfProtection, function (req, res) {
     var transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
-            user: 'hexschool@gmail.com',
+            user: 'hexschooltest6@gmail.com',
             pass: 'edith1004'
         }
     })
     var mailOptions = {
         from: '"六角學院"<service@hexschool.com>',
-        to: 'hexschoolnode@gmail.com',
+        to: 'afwyousenge@gmail.com',
         subject: req.body.username + '記了一封信',
         text: req.body.description
     }
